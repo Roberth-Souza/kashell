6223f2426d0747d9f23a8362b9f2acf6f1e0c51d
RobeTech <roberth.halk@gmail.com>
2026-05-27 11:22:43 -0300

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ def main():
         sys.stdout.write("$ ")
         command = input()
         if command == "exit":
-            return
+            break
         print(f"{command}: command not found")
         continue
 
