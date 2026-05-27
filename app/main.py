934cc9aff444cf179442559917a0b5d833ac3b3a
RobeTech <roberth.halk@gmail.com>
2026-05-27 11:30:50 -0300

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -5,10 +5,16 @@ def main():
     while True:
         sys.stdout.write("$ ")
         command = input()
-        if command == "exit":
+
+        if command.split()[0] == "echo":
+            print(command[5:])
+
+        elif command == "exit":
             break
-        print(f"{command}: command not found")
-        continue
+
+        else:
+            print(f"{command}: command not found")
+            continue
 
 
 if __name__ == "__main__":
